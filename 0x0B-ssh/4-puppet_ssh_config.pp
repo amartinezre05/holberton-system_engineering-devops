de790fbec24fa0c3f71d7afb053f52ba0dde1e84
amartinezre05 <almartinezre05@gmail.com>
2019-12-10 22:34:54 +0000

    [add] configuration file with puppet

---
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -0,0 +1,11 @@
+# Client configuration file (w/ Puppet) 
+file_line { 'Append a line to /etc/ssh/ssh_config':
+ensure => present,
+path   => '/etc/ssh/ssh_config',
+line   => '    PasswordAuthentication no',
+} 
+file_line { 'Append a line to /etc/fuse.conf':
+ensure => present,
+path   => '/etc/ssh/ssh_config',
+line   => '    IdentityFile ~/.ssh/holberton',
+}
