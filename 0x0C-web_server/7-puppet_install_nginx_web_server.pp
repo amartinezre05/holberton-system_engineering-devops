64d50c543a4cc2de492a934b5d297f17c732d7af
amartinezre05 <almartinezre05@gmail.com>
2020-03-29 15:30:04 -0500

    [add] puppet to install nginx

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,5 @@
+#  Install Nginx web server
+exec { 'config':
+  provider => shell,
+  command  => 'sudo apt-get update; sudo apt-get -y install nginx; echo "Holberton School" > /var/www/html/index.nginx-debian.html; sed -i '48i location /redirect_me{\nrewrite ^/(.*)$ https://www.youtube.com/watch?v=XPwo6giiZ_o permanent;\n}\n' /etc/nginx/sites-available/default; sudo service nginx restart',
+}
